0272e54e3aece3dd13b026c5e34d0ad2cb40ac13
priyabanait <priyabanait91@gmail.com>
2023-04-01 14:10:16 +0530

    Expence Item using react

---
+++ src/Expenceitem.js
@@ -0,0 +1,18 @@
+
+
+function ExpenceItem(){
+   return  (
+   
+    <div>
+        <h2>Expence Items</h2>
+        <div>
+            <ul>
+                <li>Food <span>&#8377;</span>10</li>
+                <li>Petrol <span>&#8377;</span>100</li>
+                <li>Movies <span>&#8377;</span>200</li>
+            </ul>
+    </div>
+      </div>
+   )
+}
+export default ExpenceItem;
