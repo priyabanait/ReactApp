87afbbeb970ca561196f197430fdb57c541dbf8e
priyabanait <priyabanait91@gmail.com>
2023-04-03 19:46:16 +0530

    Passing Data

--- src/Expenceitem.js
+++ src/Expenceitem.js
@@ -1,17 +1,18 @@
+import './ExpenceItem.css';
 
-
-function ExpenceItem(){
+function ExpenceItem( LocationOfExpenditure){
    return  (
-   <div>
-        <h2>Expence Items</h2>
-        <div>
-            <ul>
-                <li>Food <span>&#8377;</span>10</li>
-                <li>Petrol <span>&#8377;</span>100</li>
-                <li>Movies <span>&#8377;</span>200</li>
-            </ul>
-    </div>
+
+  
+   
+    <div className="expence_item">
+        <div>{ LocationOfExpenditure.date.toISOString()}</div>   
+        <div className="expence-item_description"> 
+        <h2>{ LocationOfExpenditure.title}</h2>
+        </div>
+        <div className="expence-item_price">${ LocationOfExpenditure.amount}</div>
       </div>
+      
    )
 }
 export default ExpenceItem;
