11bff279b867701b08a580871020183265f06976
priyabanait <priyabanait91@gmail.com>
2023-04-04 00:27:06 +0530

    getting data using loop

--- src/Expenceitem.js
+++ src/Expenceitem.js
@@ -1,16 +1,16 @@
 import './ExpenceItem.css';
 
-function ExpenceItem( LocationOfExpenditure){
+function ExpenceItem( props){
    return  (
 
   
    
     <div className="expence_item">
-        <div>{ LocationOfExpenditure.date.toISOString()}</div>   
+        <div>{ props.date.toISOString()}</div>   
         <div className="expence-item_description"> 
-        <h2>{ LocationOfExpenditure.title}</h2>
+        <h2>{ props.title}</h2>
         </div>
-        <div className="expence-item_price">${ LocationOfExpenditure.amount}</div>
+        <div className="expence-item_price">${ props.amount}</div>
       </div>
       
    )
