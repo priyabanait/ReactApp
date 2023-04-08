2058b5f20fcc0f67016dd738bddea7833d8effa2
priyabanait <priyabanait91@gmail.com>
2023-04-08 20:05:33 +0530

    Manipulating the DOM - React States

--- src/components/Expenceitem.js
+++ src/components/Expenceitem.js
@@ -1,29 +1,29 @@
+import React, {useState} from 'react';
 import './ExpenceItem.css';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 
-const ExpenceItem=( props)=>{
-   const deletebtn=()=>{
-      let a=document.getElementById("object");
-       a.innerHTML = "";
-      console.log("deleted");
-   }
+let ExpenceItem=( props)=>{
+  
+   let [amount, setAmount]=useState(props.amount);
+   let ChangeAmount=()=>{
+      setAmount('100');
+      console.log(amount);
+   };
    return  (
-  <>
-   <ul id="object">
-     <li className="expence-item">
+
+     <div className="expence-item">
      <ExpenseDate date={props.date}></ExpenseDate>
          
         <div className="expence-item_description"> 
        <h2><ExpenseDetails title={props.title}></ExpenseDetails></h2>
         </div>
         <div className="expence-item_price">
-       <h3>$<ExpenseDetails amount={props.amount}></ExpenseDetails></h3> 
+       <h3>$<ExpenseDetails amount={amount}></ExpenseDetails></h3> 
          </div>
-         <button onClick={deletebtn}>Delete Expence</button>
-      </li>
-      </ul>
-     </>
+         <button onClick={ChangeAmount}>Change Expense</button>
+      </div>
+     
    )
 }
 export default ExpenceItem;
