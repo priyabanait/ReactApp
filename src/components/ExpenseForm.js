a9181fa54a3d135033c473e4aa34794a46cacf82
priyabanait <priyabanait91@gmail.com>
2023-04-09 16:27:43 +0530

    Lets introduce state inside the new Component

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,25 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react'
+
 
 export default function ExpenseForm() {
-    function display(event){
-        event.preventDefault();
-       const show=event.target.value;
-         console.log(show);
+
+  const[enteredtitle ,setenteredtitle]=useState('');
+  const[enteredAmount,setenteredAmount]=useState('');
+  const[enteredDate ,setenteredDate]=useState('');
+
+    function displayTitle(event){
+      setenteredtitle(event.target.value);
         }
+
+        function displayAmount(event){
+          setenteredAmount(event.target.value);
+          }
+
+          function displayDate(event){
+            setenteredDate(event.target.value);
+            }
   return (
     <form>
       <div className='newExpense'>
            <div className='title'>
             <label>Title: </label>
-            <input type='text' onChange={display}></input>
+            <input type='text' onChange={displayTitle}></input>
            </div>
            <div className='amount'>
             <label>Amount: </label>
-            <input type='number' onChange={display}></input>
+            <input type='number' onChange={displayAmount}></input>
            </div>
            <div className='date'>
             <label>Date: </label>
-            <input type='date' min="2020-1-1" max="2022-12-31" onChange={display}></input>
+            <input type='date' min="2020-1-1" max="2022-12-31" onChange={displayDate}></input>
            </div>
       </div>
       <div>
